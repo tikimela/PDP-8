0165cd1832396c43b223f5f42482fef54b2848df
Mirela Abazovic <70277304+tikimela@users.noreply.github.com>
2022-06-09 19:38:08 +0200

    Setup

---
+++ cmake-build-debug/CMakeFiles/PDP_8.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PDP_8.dir/src/main.c.obj"
+  "PDP_8.exe"
+  "PDP_8.exe.manifest"
+  "PDP_8.pdb"
+  "libPDP_8.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PDP_8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
